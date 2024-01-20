17435231337cbb6eab6c708c439dcb05b9fad353
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2024-01-20 14:26:35 -0300

    requisicao post

---
+++ js/buscarVideo.js
@@ -0,0 +1,30 @@
+import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js";
+
+async function buscarVideo(evento) {
+  evento.preventDefault();
+
+  const dadosPesquisa = document.querySelector("[data-pesquisa]").value;
+  const busca = await conectaApi.buscaVideo(dadosPesquisa);
+
+  const lista = document.querySelector("[data-lista]");
+
+  while (lista.firstChild) {
+    lista.removeChild(lista.firstChild);
+  }
+
+  busca.forEach((elemento) =>
+    lista.appendChild(
+      constroiCard(
+        elemento.titulo,
+        elemento.descricao,
+        elemento.url,
+        elemento.imagem
+      )
+    )
+  );
+}
+
+const botaoPesquisa = document.querySelector("[data-botao-pesquisa]");
+
+botaoPesquisa.addEventListener("click", (evento) => buscarVideo(evento));
