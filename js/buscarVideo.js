966c6121236c7c444f5befc67ffdcc05b7143477
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2024-01-23 21:01:24 -0300

    tratamendo de erros

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -2,20 +2,31 @@ import { conectaApi } from "./conectaApi.js";
 import constroiCard from "./mostrarVideos.js";
 
 async function buscarVideo(evento) {
-    evento.preventDefault();
-    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
-    const busca = await conectaApi.buscaVideo(dadosDePesquisa);
+  evento.preventDefault();
+  const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
+  const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
-    const lista = document.querySelector("[data-lista]");
+  const lista = document.querySelector("[data-lista]");
 
-    while (lista.firstChild) {
-        lista.removeChild(lista.firstChild);
-    }
+  while (lista.firstChild) {
+    lista.removeChild(lista.firstChild);
+  }
 
-    busca.forEach(elemento => lista.appendChild(
-        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+  busca.forEach((elemento) =>
+    lista.appendChild(
+      constroiCard(
+        elemento.titulo,
+        elemento.descricao,
+        elemento.url,
+        elemento.imagem
+      )
+    )
+  );
+  if (busca.length == 0) {
+    lista.innerHTML = `<h2 class="mensagem__titulo"> Não existem vídeos com esse termo</h2>`;
+  }
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
 
-botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento))
+botaoDePesquisa.addEventListener("click", (evento) => buscarVideo(evento));
